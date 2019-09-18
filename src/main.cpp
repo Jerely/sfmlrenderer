2d74f237aadee80fc11575c790cf6e291247d3c4
elliot <misha9f@gmail.com>
2019-09-18 20:30:08 +0300

    Я умею рисовать линии.

--- src/main.cpp
+++ src/main.cpp
@@ -12,21 +12,6 @@ const float FOV = 90.0f;
 const int SCALEW = 4;
 const int SCALEH = 4;
 
-/*
-plotLine(x0,y0, x1,y1)
-  dx = x1 - x0
-  dy = y1 - y0
-  D = 2*dy - dx
-  y = y0
-
-  for x from x0 to x1
-    plot(x,y)
-    if D > 0
-       y = y + 1
-       D = D - 2*dx
-    end if
-    D = D + 2*dy
-*/
 
 void displayBitmap(sf::RenderWindow& window, uint8_t* bitmap) {
     static sf::Image image;
@@ -42,23 +27,67 @@ void displayBitmap(sf::RenderWindow& window, uint8_t* bitmap) {
     window.display();
 }
 
-void bresenham(uint32_t x0, uint32_t y0, uint32_t x1, uint32_t y1, uint8_t* bitmap) {
+void plotLineLow(int x0, int y0, int x1, int y1, uint8_t* bitmap) {
     int dx = x1 - x0;
     int dy = y1 - y0;
+    int yi = 1;
+    if (dy < 0) {
+      yi = -1;
+      dy = -dy;
+    }
     int D = 2*dy - dx;
     int y = y0;
     for(int x = x0; x <= x1; ++x) {
         setPixel(bitmap, x, y, WHITE);
         if(D > 0) {
-            ++y;
-            D -= 2*dx;
+            y = y + yi;
+            D = D - 2*dx;
         }
-        D += 2*dy;
+      D += 2*dy;
+    }
+}
+
+void plotLineHigh(int x0, int y0, int x1, int y1, uint8_t* bitmap) {
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int xi = 1;
+    if(dx < 0) {
+        xi = -1;
+        dx = -dx;
+    }
+    int D = 2*dx - dy;
+    int x = x0;
+    for(int y = y0; y <= y1; ++y) {
+        setPixel(bitmap, x, y, WHITE);
+        if(D > 0) {
+            x += xi;
+            D -= 2*dy;
+        }
+        D += 2*dx;
     }
 }
 
 
+#define abs(x) ((x) > 0 ? (x) : -(x))
 
+void plotLine(int x0, int y0, int x1, int y1, uint8_t* bitmap) {
+    if(abs(y1 - y0) < abs(x1 - x0)) {
+        if(x0 > x1) {
+            plotLineLow(x1, y1, x0, y0, bitmap);
+        }
+        else {
+            plotLineLow(x0, y0, x1, y1, bitmap);
+        }
+    }
+    else {
+        if(y0 > y1) {
+            plotLineHigh(x1, y1, x0, y0, bitmap);
+        }
+        else {
+            plotLineHigh(x0, y0, x1, y1, bitmap);
+        }
+    }
+}
 
 int main()
 {
@@ -76,7 +105,7 @@ int main()
         }
      
         fillPixels(bitmap, BLACK);
-        bresenham(0, 0, 100, 50, bitmap);
+        plotLine(100, 50, 50, 75, bitmap);
         displayBitmap(window, bitmap);
         //scene.draw(window, bitmap);
     }
